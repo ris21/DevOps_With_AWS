8b490ae1873968fc51a609dcb5655f98b745fa48
Rajan Dubey <rajandubey200@gmail.com>
2025-02-28 20:29:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
   region     = "eu-west-2"
-  profile    = "default"
   access_key = "AKIAWIJIUVESO5KIVEVW"
   secret_key = "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"
 }
-
 resource "aws_instance" "ServerA" {
   ami           = "ami-0fd24edaae77b2388"
   instance_type = "t2.micro"
